39e4b7ed48fcf030237e02f1a5c8dfdccc2dcd3e
Craig Adam <cadam@telpay.ca>
2016-01-20 13:05:40 -0600

    Basically working now

--- parse.py
+++ parse.py
@@ -2,31 +2,49 @@
 import argparse
 import csv
 
+account_types = ['Chequing', 'MasterCard']
+output_fields = ['Account', 'Date', 'Description', 'Category', 'Amount']
+
 def main():
 	parser = argparse.ArgumentParser(description='Command line utility for parsing RBC transaction export csv files')
-	parser.add_argument('inputFile', help='The name of the csv file to parse')
+	parser.add_argument('inputfile', help='The name of the csv file to parse')
+	parser.add_argument('-O', '--outputfile', default='out.csv', help='The filename for writing results')
 
 	args = parser.parse_args()
 
-	with open(args.inputFile) as csvFile:
-		dialect = csv.Sniffer().sniff(csvFile.read(1024))
-		csvFile.seek(0)
-		reader = csv.reader(csvFile, dialect)
+	with open(args.inputfile) as infile, open(args.outputfile, 'w') as outfile:
+		reader = csv.DictReader(infile)
+		writer = csv.DictWriter(outfile, output_fields, quoting=csv.QUOTE_NONNUMERIC)
+		writer.writeheader()
+
 		for row in reader:
-			parseRow(row)
-		csvFile.close()
+			newrow = parserow(row)
+			if newrow:
+				writer.writerow(newrow)
+
+		infile.close()
+		outfile.close()
 
 
 
-#------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 
-def parseRow(row):
-	print ' -- '.join(row)
+def parserow(row):
+	if row['Account Type'] in account_types:
+		amount = float(row['CAD$']) * -1
+		newrow = {
+			'Account': row['Account Type'],
+			'Date': row['Transaction Date'],
+			'Description': row['Description 1'].title(),
+			'Category': '',
+			'Amount': amount,
+		}
+		return newrow
 
 
 
 
-#------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
 
 if __name__ == "__main__":
     main()	
